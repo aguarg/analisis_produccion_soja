58e5e999587a88a486b36145303de861aff56a62
aguarg <93261481+aguarg@users.noreply.github.com>
2021-12-24 09:57:00 -0300

    Add files via upload
    Agregado los datos de todo el csv y mejorado el layout general

--- soja_anual.py
+++ soja_anual.py
@@ -2,19 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#data_soja = pd.read_csv("soja-anual.csv", header=0, sep=",", usecols=['indice_tiempo', 'superficie_cosechada_soja_ha'])
+#=========================================
+# ÍNIDICE
+# 1- EXTRAYENDO LOS DATOS DE LOS CAMPOS
+# 2- TRATAMIENTO ESTADÍSTICO DE LOS DATOS
+# 3- GRÁFICAS
+#=========================================
 
-# EXTRAYENDO LOS DATOS DE LOS CAMPOS:
+
+
+#========================================================================================================================
+# 1- EXTRAYENDO LOS DATOS DE LOS CAMPOS:
+# Extraer los datos de todos los campos:
+data_soja = pd.read_csv("soja-anual.csv", header=0, sep=",")
+
+
 # Extraer los datos de la columna produccion_soja:
 prod_soja = pd.read_csv("soja-anual.csv", header=0, sep=",", usecols=["produccion_soja_t"])
 
-# Superficie cosechada:
+# Extraer los datos de la columna Superficie cosechada:
 superficie_cosechada = pd.read_csv("soja-anual.csv", header=0, sep=",", usecols=["superficie_cosechada_soja_ha"])
 
-# Superficie sembrada:
+# Extraer los datos de la columna Superficie sembrada:
 superficie_sembrada = pd.read_csv("soja-anual.csv", header=0, sep=",", usecols=["superficie_sembrada_soja_ha"])
 
-# Rendimiento:
+# Extraer los datos de la columna Rendimiento:
 rendimiento = pd.read_csv("soja-anual.csv", header=0, sep=",", usecols=["rendimiento_soja_kgxha"])
 
 # Extraer los datos de la columna tiempo:
@@ -24,25 +36,57 @@ tiempo = pd.read_csv("soja-anual.csv", header=0, sep=",", usecols=["indice_tiemp
 
 
 
-# GRÁFICAS:
+
+#=========================================================================================================================
+# 2- TRATAMIENTO ESTADÍSTICO:
+
+
+
+
+
+
+
+
+
+
+#=========================================================================================================================
+# 3- GRÁFICAS:
 # Gráfica 1: producción vs tiempo:
-plt.subplot(2, 1, 1)
-plt.plot(tiempo, prod_soja)
-plt.title("Producción de soja")
+plt.subplot(2, 2, 1)
+plt.plot(tiempo, prod_soja, color="orange")
+plt.title("Producción de soja [tn/año]")
 plt.xlabel("Año")
 plt.ylabel("Producción [tn]")
 
 
 # Grafico 2: superficie cosechada vs tiempo:
-plt.subplot(2, 1, 2)
+plt.subplot(2, 2, 2)
 plt.plot(tiempo, superficie_cosechada)
-plt.title("Superficie cosechada")
+plt.title("Superficie cosechada [ha]")
 plt.xlabel("Año")
 plt.ylabel("Superficie [ha]")
 
 
-# genera espacio vertical entre los subplots para que no queden encimadas:
-plt.tight_layout(h_pad=2.0) 
+# Gráfico 3: superficie sembrada vs tiempo:
+plt.subplot(2, 2, 3)
+plt.plot(tiempo, superficie_sembrada, color="green")
+plt.title("Superficie sembrada [ha]")
+plt.xlabel("Año")
+plt.ylabel("Superficie [ha]")
+
+
+# Gráfico 4: rendimiento vs tiempo:
+plt.subplot(2, 2, 4)
+plt.plot(tiempo, rendimiento, color="red")
+plt.title("Rendimiento")
+plt.xlabel("Año")
+plt.ylabel("Rendimiento [kg/ha]")
+
+
+
+# genera padding vertical entre los subplots para que no queden encimadas:
+plt.tight_layout(pad=2.0)
+
 
 
 
