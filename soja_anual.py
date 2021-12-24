cb1e77fc7e52826d43f0da403eafc0eea845118d
aguarg <93261481+aguarg@users.noreply.github.com>
2021-12-24 13:07:58 -0300

    Add files via upload
    Agregado el cálculo de la media, el desvío estándar, los valores máximos y mínimos.

--- soja_anual.py
+++ soja_anual.py
@@ -39,7 +39,51 @@ tiempo = pd.read_csv("soja-anual.csv", header=0, sep=",", usecols=["indice_tiemp
 
 #=========================================================================================================================
 # 2- TRATAMIENTO ESTADÍSTICO:
+# hacer que los resultados se guarden en un archivo de texto.
+# Ver si se puede presentar como una tabla linda. Pandas o numpy pueden hacer eso
+# Lo mismo con el archivo generado. Si es en PDF se puede dar un formato mejor.
+# Ver si conviene hacerlo con quo
 
+
+# Número total de datos
+tamano_dataframe = data_soja.size
+print("Tamaño del dataframe: " + str(tamano_dataframe))
+
+
+
+# Medias:
+prod_soja_promedio = np.mean(prod_soja)
+superficie_cosechada_promedio = np.mean(superficie_cosechada)
+superficie_sembrada_promedio = np.mean(superficie_sembrada)
+rendimiento_promedio = np.mean(rendimiento)
+
+
+# Desvío estándar
+std_prod_soja = np.std(prod_soja)
+std_superficie_cosechada = np.std(superficie_cosechada)
+std_superficie_sembrada = np.std(superficie_sembrada)
+std_rendimiento = np.std(rendimiento)
+
+
+# Valor mínimo
+min_prod_soja = np.min(prod_soja)
+min_superficie_cosechada = np.min(superficie_cosechada)
+min_superficie_sembrada = np.min(superficie_sembrada)
+min_rendimiento = np.min(rendimiento)
+
+# Valor máximo
+max_prod_soja = np.max(prod_soja)
+max_superficie_cosechada = np.max(superficie_cosechada)
+max_superficie_sembrada = np.max(superficie_sembrada)
+max_rendimiento = np.max(rendimiento)
+
+# Percentiles
+
+# 25%
+
+# 50%
+
+# 75%
 
 
 
@@ -57,6 +101,7 @@ plt.plot(tiempo, prod_soja, color="orange")
 plt.title("Producción de soja [tn/año]")
 plt.xlabel("Año")
 plt.ylabel("Producción [tn]")
+
 
 
 # Grafico 2: superficie cosechada vs tiempo:
@@ -89,5 +134,5 @@ plt.tight_layout(pad=2.0)
 
 
 
-
+plt.suptitle("Producción argentina de soja, período 1969-2019")
 plt.show()
